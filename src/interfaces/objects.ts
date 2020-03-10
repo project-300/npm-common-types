cde261a05789f82acaa4e55714c1c859ebd9cb09
Brian Keaveney <38507940+BrianKeaveney@users.noreply.github.com>
2020-03-10 21:38:30 +0000

    user now extends db item as well

--- src/interfaces/objects.ts
+++ src/interfaces/objects.ts
@@ -38,7 +38,7 @@ export interface UserConnection {
 	connectedAt: string;
 }
 
-export interface User extends UserBrief {
+export interface User extends UserBrief, DBItem {
 	email: string;
 	phone: string;
 	times: {
